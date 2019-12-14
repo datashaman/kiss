3738c363b9ce741da5f38258af875bc7cc4edcf5
datashaman <marlinf@datashaman.com>
2019-12-14 12:46:28 +0200

    wip

--- resources/js/components/Mentions.vue
+++ resources/js/components/Mentions.vue
@@ -51,7 +51,7 @@ export default {
     },
     methods: {
         vagueTime: function (dt) {
-            console.log('dt = ' + new Date(dt))
+            console.log('dt = ' + dt)
             return vagueTime.get({ to: new Date(dt) })
         }
     }
